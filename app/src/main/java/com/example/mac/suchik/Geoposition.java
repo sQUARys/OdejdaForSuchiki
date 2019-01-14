29c5732488c377431693490e7abcc7b4c47cb527
mkrivileva <maria.krivileva13@gmail.com>
2019-01-14 11:12:17 +0300

    Get current GPS location

---
+++ app/src/main/java/com/example/mac/suchik/Geoposition.java
@@ -0,0 +1,105 @@
+package com.example.mac.suchik;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+
+
+public class Geoposition {
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
+    private static Context mContext;
+
+
+    public Geoposition(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public String[] start(){
+        locationManager = (LocationManager)mContext.getSystemService(Context.LOCATION_SERVICE);
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+            return new String[]{"", ""};
+        } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            return getLocation();
+        } else return new String[]{"notProviderEnabled", "notProviderEnabled"};
+
+    }
+
+
+    private String[] getLocation() {
+        String[] position = new String[2];
+        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions((Activity) mContext, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+
+        }
+        else{
+
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            Location location2 = locationManager.getLastKnownLocation(LocationManager. PASSIVE_PROVIDER);
+
+            if (location != null) {
+                double latti = location.getLatitude();
+                double longi = location.getLongitude();
+                position[0] = String.valueOf(latti);
+                position[1] = String.valueOf(longi);
+
+                //textView.setText("Your current location is"+ "\n" + "Lattitude = " + lattitude
+                //        + "\n" + "Longitude = " + longitude);
+
+            } else  if (location1 != null) {
+                double latti = location1.getLatitude();
+                double longi = location1.getLongitude();
+                position[0] = String.valueOf(latti);
+                position[1] = String.valueOf(longi);
+
+            } else  if (location2 != null) {
+                double latti = location2.getLatitude();
+                double longi = location2.getLongitude();
+                position[0] = String.valueOf(latti);
+                position[1] = String.valueOf(longi);
+
+            } else{
+
+                position[0] = "null";
+                position[1] = "null";
+
+            }}
+        return  position;
+    }
+
+    protected void buildAlertMessageNoGps() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setMessage("Please Turn ON your GPS Connection")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        mContext.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
+
