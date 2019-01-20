a591cec0080e710fe6830fd32976ccabe1aae3cb
Roman <roman.kocenko.2004@mail.ru>
2019-01-20 12:59:00 +0200

    Update MainActivityUI, who changed a weather

--- app/src/main/java/com/example/mac/suchik/UI/MainWindowFragment.java
+++ app/src/main/java/com/example/mac/suchik/UI/MainWindowFragment.java
@@ -7,11 +7,10 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,27 +22,24 @@ import com.example.mac.suchik.ResponseType;
 import com.example.mac.suchik.Storage;
 import com.example.mac.suchik.UI.main_window.ClickRecomendationListAdapter;
 import com.example.mac.suchik.UI.main_window.RecomendationListAdapter;
-import com.example.mac.suchik.UI.main_window.VH;
-import com.example.mac.suchik.UI.settings_page.TimesListAdapter;
 import com.example.mac.suchik.WeatherData.Fact;
 
-import org.w3c.dom.Text;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainWindowFragment extends Fragment implements Callbacks {
     public static final int SCHEDULE_WINDOW_FRAGMENT = 1;
-
     private Storage mStorage;
-
     private TextView city_name;
-    private TextView time;
+    private TextView date;
     private ImageView weather_cloud;
     private TextView temperature;
     private TextView weather_cloud__description;
-
     RecomendationListAdapter recomendationListAdapter;
 
     @Nullable
@@ -51,6 +47,7 @@ public class MainWindowFragment extends Fragment implements Callbacks {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mStorage = Storage.getOrCreate(null); // null потому что я надеюсь, что Storage уже инициализирован
         return inflater.inflate(R.layout.main_window, container, false);
+
     }
 
     @Override
@@ -70,17 +67,20 @@ public class MainWindowFragment extends Fragment implements Callbacks {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        time = view.findViewById(R.id.time);
         city_name = view.findViewById(R.id.city_name);
-        weather_cloud = view.findViewById(R.id.weather_cloud);
         temperature = view.findViewById(R.id.temperature);
+        weather_cloud = view.findViewById(R.id.weather_cloud);
+        date = view.findViewById(R.id.date);
         weather_cloud__description = view.findViewById(R.id.weather_cloud__description);
         RecyclerView rv = view.findViewById(R.id.recommendation_list);
         rv.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         List<Clothe> data = new ArrayList<>();
         recomendationListAdapter = new ClickRecomendationListAdapter(data, null);
         rv.setAdapter(recomendationListAdapter);
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        Date currentDate = new Date();
+        String dateText = dateFormat.format(currentDate);
+        date.setText(dateText);
     }
 
     public void onWeatherDataUpdated(Fact weather) {
@@ -89,6 +89,66 @@ public class MainWindowFragment extends Fragment implements Callbacks {
         else if (weather.getTemp() < 0)
             temperature.setText(String.format("-" + "%f" , weather.getTemp()));
         else temperature.setText("0");
+    }
+    public void onChangedWeatherDraw(Fact weather){
+        String condition = weather.getCondition();
+        Log.d("weather", condition);
+        switch (condition){
+                case "clear":
+                weather_cloud.setImageResource(R.drawable.sunny);
+                break;
+                case "partly-cloudy ":
+                weather_cloud.setImageResource(R.drawable.home);
+                break;
+                case "cloudy":
+                weather_cloud.setImageResource(R.drawable.home);
+                break;
+                case "overcast":
+                weather_cloud.setImageResource(R.drawable.sunny);
+                break;
+                case "partly-cloudy-and-light-rain":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "partly-cloudy-and-rain":
+                weather_cloud.setImageResource(R.drawable.rain);
+                break;
+                case "overcast-and-rain":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "overcast-thunderstorms-with-rain":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "cloudy-and-light-rain":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "overcast-and-light-rain":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "cloudy-and-rain":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "overcast-and-wet-snow":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "partly-cloudy-and-light-snow":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "partly-cloudy-and-snow":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "overcast-and-snow":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "cloudy-and-light-snow":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "overcast-and-light-snow":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+                case "cloudy-and-snow":
+                weather_cloud.setImageResource(R.drawable.snowing);
+                break;
+        }
     }
     @Override
     public void onLoad(Response response) {
@@ -100,6 +160,7 @@ public class MainWindowFragment extends Fragment implements Callbacks {
             case ResponseType.WTODAY:
                 Fact f = (Fact) response.response;
                 onWeatherDataUpdated(f);
+                onChangedWeatherDraw(f);
                 break;
         }
     }
